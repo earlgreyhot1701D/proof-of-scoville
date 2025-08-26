82baa2ccc79d08be1db04445245bba24007c7011
earlgreyhot1701D <lsjcordero@gmail.com>
2025-08-25 21:01:46 -0700

    Normalize line endings and update config files

--- app/index.js
+++ app/index.js
@@ -1,5 +1,9 @@
 import SauceEntryScreen from '../components/SauceEntryScreen';
 
+export const metadata = {
+  title: 'Sauce Entry',
+};
+
 export default function Index() {
   return <SauceEntryScreen />;
 }
