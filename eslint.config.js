f269ff8370a1afa3171a8116462fb2f1c6ea6f16
earlgreyhot1701D <lsjcordero@gmail.com>
2025-09-01 09:39:41 -0700

    chore: install ESLint plugins and replace config

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,83 @@
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+// eslint.config.js
 
-module.exports = defineConfig([
-  expoConfig,
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import eslintPluginImport from 'eslint-plugin-import';
+import eslintPluginJsxA11y from 'eslint-plugin-jsx-a11y';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintPluginReact from 'eslint-plugin-react';
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
+import globals from 'globals';
+
+export default [
   {
-    ignores: ['dist/*'],
-  },
-  {
+    ignores: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/build/**',
+      '**/__tests__/**',
+      '**/coverage/**',
+    ],
+    files: ['**/*.{js,ts,jsx,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+        project: './tsconfig.json',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      react: eslintPluginReact,
+      'react-hooks': eslintPluginReactHooks,
+      'jsx-a11y': eslintPluginJsxA11y,
+      import: eslintPluginImport,
+      prettier: eslintPluginPrettier,
+      '@typescript-eslint': tseslint,
+    },
     rules: {
-      // This works *because* the plugin is already loaded by Expo
-      'import/extensions': [
-        'error',
-        'ignorePackages',
+      // Prettier integration
+      'prettier/prettier': 'warn',
+
+      // React
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+
+      // Hooks
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+
+      // A11y
+      'jsx-a11y/anchor-is-valid': 'warn',
+
+      // Import sorting
+      'import/order': [
+        'warn',
         {
-          js: 'never',
-          jsx: 'never',
-          ts: 'never',
-          tsx: 'never',
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+          alphabetize: { order: 'asc', caseInsensitive: true },
         },
       ],
+
+      // TypeScript
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
-]);
+];
+
